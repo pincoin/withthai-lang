e0e35157e9516a5f24611ffdf757a0491cdab260
pincoin <pincoins@gmail.com>
2018-07-17 22:50:28 +0900

    - admin title

--- rakmai/admin.py
+++ rakmai/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+admin.site.site_header = _('WITHTHAI admin')
+admin.site.site_title = _('WITHTHAI admin')
+admin.site.index_title = _('WITHTHAI administration')
