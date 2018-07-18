d455202e604b8d2680ae6467c589687292f2cfc7
pincoin <pincoins@gmail.com>
2018-07-18 20:07:53 +0900

    - caching

--- help/templatetags/help_tags.py
+++ help/templatetags/help_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.conf import settings
+from django.core.cache import cache
 
 from ..models import NoticeMessage
 
@@ -7,5 +9,12 @@ register = template.Library()
 
 @register.simple_tag
 def get_notice(count=5):
-    notices = NoticeMessage.objects.all().order_by('-created')[:count]
+    cache_key = 'voca.templatetags.voca_tags.get_textbook()'
+    cache_time = settings.CHACHE_TIME_SHORT
+
+    notices = cache.get(cache_key)
+    if not notices:
+        notices = NoticeMessage.objects.all().order_by('-created')[:count]
+        cache.set(cache_key, notices, cache_time)
+
     return notices
