924c8cd44e1de0b7dc11f5abd2deb7a29932d57b
pincoin <pincoins@gmail.com>
2018-07-19 22:35:46 +0900

    - add page button

--- book/forms.py
+++ book/forms.py
@@ -1,3 +1,7 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import (
+    Layout, Fieldset, Div, Submit
+)
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from mptt.forms import TreeNodeChoiceField
@@ -19,6 +23,38 @@ class PageForm(forms.ModelForm):
         self.fields['parent'].required = False
         self.fields['parent'].label = _('parent')
 
+        self.helper = FormHelper()
+        self.helper.include_media = False
+
+        # horizontal grid form
+        self.helper.form_class = 'form-horizontal'  # Appends `row` class to `div` surrounding label and form field
+        self.helper.label_class = 'col-2 col-form-label'
+        self.helper.field_class = 'col-10'
+
+        # `category` field is selectively hidden.
+        fieldset = [
+            '',  # Hide the legend of fieldset (HTML tag)
+            'title',
+            'parent',
+            'content',
+            'content1',
+            'content2',
+            'image',
+            'youtube',
+            'status',
+            'keywords',
+            'description',
+        ]
+
+        self.helper.layout = Layout(
+            Fieldset(*fieldset),
+            Div(
+                Submit('submit', _('Write'), css_class='btn btn-info btn-block'),
+                css_class='my-3',
+            )
+        )
+
     class Meta:
         model = Page
-        fields = ('title', 'parent', 'content', 'content1', 'content2', 'keywords', 'description', 'status')
+        fields = ('title', 'parent', 'image', 'youtube', 'content', 'content1', 'content2',
+                  'keywords', 'description', 'status')
