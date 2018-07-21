ec01b000513ef07c468874204fbb34beb94bface
pincoin <pincoins@gmail.com>
2018-07-21 21:14:27 +0900

    - thai-font js

--- rakmai/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    // beautify thai
-    document.body.innerHTML = document.body.innerHTML.replace(
-        /([\u0E00-\u0E7F]+)/g,
-        function (x) {
-            return '<span class="thai-font">' + x + '</span>'
-        }
-    );
-});
