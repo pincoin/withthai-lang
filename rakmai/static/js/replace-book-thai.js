92e6a01431cbeba834cc1c217b0f81d8b4bf0304
pincoin <pincoins@gmail.com>
2018-07-21 21:33:50 +0900

    - replace thai

--- rakmai/static/js/replace-book-thai.js
+++ rakmai/static/js/replace-book-thai.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    // beautify thai
-    document.body.innerHTML = document.body.innerHTML.replace(
+    let content1 = $('#content1');
+
+    content1.html(content1.html().replace(
         /([\u0E00-\u0E7F]+)/g,
         function (x) {
             return '<span class="thai-font">' + x + '</span>'
         }
-    );
+    ));
 });
