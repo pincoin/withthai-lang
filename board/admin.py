ef35d51ccd01f4a1f56cb50124f0b9e1b02ee835
pincoin <pincoins@gmail.com>
2018-07-25 21:05:27 +0900

    - admin page for board app

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Board, Category, Attachment, Message
+)
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'theme', 'allow_comments', 'chunk_size', 'block_size')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
+admin.site.register(Message, MessageAdmin)
