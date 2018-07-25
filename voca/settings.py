3f62eeb40801520db88560b5b0695e4b57b58db7
pincoin <pincoins@gmail.com>
2018-07-25 11:44:02 +0900

    - chunk size

--- voca/settings.py
+++ voca/settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 VOCA_BLOCK_SIZE = getattr(settings, 'VOCA_BLOCK_SIZE', 5)
 
 # VOCA Pagination Chunk Size
-VOCA_CHUNK_SIZE = getattr(settings, 'VOCA_CHUNK_SIZE', 40)
+VOCA_CHUNK_SIZE = getattr(settings, 'VOCA_CHUNK_SIZE', 30)
