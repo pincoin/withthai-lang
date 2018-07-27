0ae3a7418af6f05dc90dfc91a2f4a9b9bd539cd2
pincoin <pincoins@gmail.com>
2018-07-27 21:25:36 +0900

    - folding

--- rakmai/static/js/main.js
+++ rakmai/static/js/main.js
@@ -2,4 +2,24 @@ $(document).ready(function () {
     $('#id_part').on('change', function (e) {
         this.form.submit();
     });
+
+    function toggle_text(item) {
+        if (item.text() === '[펼치기]') {
+            item.text('[접기]');
+        } else {
+            item.text('[펼치기]');
+        }
+    }
+
+    $('#collapseLink1').on('click', "small", function (e) {
+        toggle_text($(this));
+    });
+
+    $('#collapseLink2').on('click', "small", function (e) {
+        toggle_text($(this));
+    });
+
+    $('#collapseLink3').on('click', "small", function (e) {
+        toggle_text($(this));
+    });
 });
